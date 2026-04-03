26ee1cb11bf557ee4c6b6a6d9bf774c0068b2e9b
swap <swapnilkkhewale@gmail.com>
2026-04-03 15:57:40 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRouters from "./routes/message.route.js"
+import connectDB from "./lib/db.js"
 
 dotenv.config()
 
@@ -11,6 +12,8 @@ const app = express()
 const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRouters)
@@ -23,4 +26,7 @@ if(process.env.NODE_ENV === "production") {
     })
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT))
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT)
+    connectDB()
+})
